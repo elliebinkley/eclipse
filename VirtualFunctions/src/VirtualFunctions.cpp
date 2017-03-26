8d660f9340bc357aaa4a3ee58e42b75217ded28f
Larry Burley <elliebinkley@gmail.com>
2017-03-26 17:46:38 -0600

    Add an exercixe that shows how virtual functions work and that constrcutors and destrcutors should not call virtual functions

---
+++ VirtualFunctions/src/VirtualFunctions.cpp
@@ -0,0 +1,41 @@
+//============================================================================
+// Name        : VirtualFunctions.cpp
+// Author      : larry Burley
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+
+struct B {
+  B() { seize(); }
+  virtual ~B() { release(); }
+
+protected:
+  virtual void seize();
+  virtual void release();
+};
+
+struct D : B {
+  virtual ~D() = default;
+
+protected:
+  void seize() override {
+    B::seize();
+    // Get derived resources...
+  }
+
+  void release() override {
+    // Release derived resources...
+    B::release();
+  }
+};
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
