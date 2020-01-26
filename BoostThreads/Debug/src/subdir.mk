5dc8b3826027480a00f5e72444e885aee87752e9
larry burley <elliebinkley@gmail.com>
2020-01-26 13:32:34 -0700

    Add files via upload

--- BoostThreads/Debug/src/subdir.mk
+++ BoostThreads/Debug/src/subdir.mk
@@ -22,8 +22,8 @@ CPP_DEPS += \
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: Cygwin C++ Compiler'
-	g++ -std=c++1y -Dboost_system -O0 -g3 -Wall -c -fmessage-length=0 -v -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++1y -Dboost_system -I"/home/laurence/eclipse/eclipse-cpp-neon-workspace" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
