3a75d12c8ec2a9c54b009db48d21656a50021707
larry burley <elliebinkley@gmail.com>
2020-01-26 16:56:44 -0700

    Deleted a file. Eclipse sees this now.

--- TemplateList/Debug/src/subdir.mk
+++ TemplateList/Debug/src/subdir.mk
@@ -4,33 +4,26 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/ListUser.cpp \
-../src/Person\ .cpp \
-../src/appUser.cpp 
+../src/AppUser.cpp \
+../src/ListTemplate.cpp \
+../src/ListUser.cpp 
 
 OBJS += \
-./src/ListUser.o \
-./src/Person\ .o \
-./src/appUser.o 
+./src/AppUser.o \
+./src/ListTemplate.o \
+./src/ListUser.o 
 
 CPP_DEPS += \
-./src/ListUser.d \
-./src/Person\ .d \
-./src/appUser.d 
+./src/AppUser.d \
+./src/ListTemplate.d \
+./src/ListUser.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: Cygwin C++ Compiler'
+	@echo 'Invoking: GCC C++ Compiler'
 	g++ -std=c++1y -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
-src/Person\ .o: ../src/Person\ .cpp
-	@echo 'Building file: $<'
-	@echo 'Invoking: Cygwin C++ Compiler'
-	g++ -std=c++1y -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/Person .d" -MT"src/Person\ .d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
